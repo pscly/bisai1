9e068f40f1a2eb015124896936305c863c1b6aca
Pscly <pscly@outlook.com>
2020-11-29 18:16:07 +0800

    修补18题，  不满足输出0

--- 18.py
+++ 18.py
@@ -8,10 +8,14 @@ a = in_1.split(s1)
 
 out_1 = [len(a) - 1, ]
 x = 0
+
 for i in a[:-1]:
     for j in i:
         x += 1
     out_1.append(x)
     x += len(s1)
-for i in out_1:
-    print(i, end=' ')
+if out_1:
+    for i in out_1:
+        print(i, end=' ')
+else:
+    print("0")
--- test.py
+++ test.py
@@ -2,13 +2,17 @@
 # 作者:Pscly
 # 创建日期: 
 # 用意：
-a = [1,2,3,4,5]
-a.pop(0)
-a.append(4)
-print(a)
+# a = [1,2,3,4,5]
+# a.pop(0)
+# a.append(4)
+# print(a)
 
 
 # for i, j in enumerate(a,1):
 #     print(i, j)
 
-print(a[:-1])
+# print(a[:-1])
+
+
+a = '123456'
+print(a.split('x'))
